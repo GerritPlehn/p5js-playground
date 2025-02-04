794483c4f7dfd0c201cbc47387958239be694e42
Gerrit Plehn <gerrit@plehn.me>
2025-02-04 21:38:49 +0100

    remove old files before rebuilding

--- build.ts
+++ build.ts
@@ -1,13 +1,15 @@
 import { watch } from 'node:fs';
+import { $ } from 'bun';
 
 const watcher = watch(
   `${import.meta.dir}/src`,
   { recursive: true },
-  (event, filename) => {
+  async (event, filename) => {
     console.log(`File ${filename} changed, rebuilding`);
     const entrypoints = Array.from(
       new Bun.Glob('src/**/index.html').scanSync()
     );
+    await $`rm -rf out`;
     Bun.build({
       entrypoints,
       outdir: 'out',
